25e8f47887904829f59cc56566e6ab5c176ef0c0
Anthony <anthonycolas.france@gmail.com>
2022-12-21 11:35:05 +0100

    Comment

--- components/LensCard.tsx
+++ components/LensCard.tsx
@@ -1,51 +1,51 @@
-import { useState, useEffect } from 'react';
-import { client, getProfile } from '../pages/api/api';
-import { Box, Image } from '@chakra-ui/react';
-import styled from '@emotion/styled';
+// import { useState, useEffect } from 'react';
+// import { client, getProfile } from '../pages/api/api';
+// import { Box, Image } from '@chakra-ui/react';
+// import styled from '@emotion/styled';
 
-const LensContainer = styled.a``;
+// const LensContainer = styled.a``;
 
-export const LensProtocole = () => {
-  const [profile, setProfile] = useState();
+// export const LensProtocole = () => {
+//   const [profile, setProfile] = useState();
 
-  useEffect(() => {
-    fetchProfile();
-  });
+//   useEffect(() => {
+//     fetchProfile();
+//   });
 
-  async function fetchProfile() {
-    try {
-      const response = await client.query(getProfile).toPromise();
-      console.log('PROFILE:', response);
-      setProfile(response.data.profile);
-    } catch (error) {
-      console.log('ERROR:', error);
-    }
-  }
+//   async function fetchProfile() {
+//     try {
+//       const response = await client.query(getProfile).toPromise();
+//       console.log('PROFILE:', response);
+//       setProfile(response.data.profile);
+//     } catch (error) {
+//       console.log('ERROR:', error);
+//     }
+//   }
 
-  return (
-    <Container
-      maxW="container.sm"
-      bg="green.400"
-      color="#262626"
-      border="2px"
-      borderColor="grey.200"
-      display="flex"
-      alignItems="center"
-      justifyContent="space-between"
-    >
-      <Image
-        height={200}
-        width={200}
-        src={profile && profile.picture.original.url}
-        borderRadius="full"
-        boxSize="150px"
-        objectFit="cover"
-        alt={profile && profile['handle']}
-      />
-      <Box>
-        <div>{profile && <div>{profile['handle']}</div>}</div>
-        <div>{profile && <div>{profile['bio']}</div>}</div>
-      </Box>
-    </Container>
-  );
-};
+//   return (
+//     <Container
+//       maxW="container.sm"
+//       bg="green.400"
+//       color="#262626"
+//       border="2px"
+//       borderColor="grey.200"
+//       display="flex"
+//       alignItems="center"
+//       justifyContent="space-between"
+//     >
+//       <Image
+//         height={200}
+//         width={200}
+//         src={profile && profile.picture.original.url}
+//         borderRadius="full"
+//         boxSize="150px"
+//         objectFit="cover"
+//         alt={profile && profile['handle']}
+//       />
+//       <Box>
+//         <div>{profile && <div>{profile['handle']}</div>}</div>
+//         <div>{profile && <div>{profile['bio']}</div>}</div>
+//       </Box>
+//     </Container>
+//   );
+// };
